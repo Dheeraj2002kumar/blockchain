cba175d4d7a5aa569bfb6cf07a93497dcd36e9a4
Dheeraj2002kumar <dk8549644@gmail.com>
2026-09-18 13:37:03 +0530

    refactor Block class and enhance output formatting; update hash calculation method

--- hash/block.py
+++ hash/block.py
@@ -1,13 +1,13 @@
 import hashlib
 
 class Block:
-    def __init__(self, data, previous_hash):
+    def __init__(self, data, previousHash):
         self.data = data
-        self.previous_hash = previous_hash
+        self.previousHash = previousHash
         self.hash = self.calculate()
 
     def calculate(self):
-        text = self.data + self.previous_hash
+        text = self.data + self.previousHash
         self.hash = hashlib.sha256(text.encode()).hexdigest()
         return self.hash
 
@@ -15,7 +15,20 @@ block1 = Block("developer", "000")
 block2 = Block("engineer", block1.hash)
 block3 = Block("software developer", block2.hash)
 
-print("Block Data: ", block1.data)
-print("Block hash: ", block1.hash)
-print("Block hash: ", block2.hash)
-print("Block hash: ", block3.hash)
+print("========== Block 1 ==========")
+print("Block Data   :", block1.data)
+print("Previous Hash:", block1.previousHash)
+print("Current Hash :", block1.hash)
+print("Hash Length  :", len(block1.hash))
+
+print("\n========== Block 2 ==========")
+print("Block Data   :", block2.data)
+print("Previous Hash:", block2.previousHash)
+print("Current Hash :", block2.hash)
+print("Hash Length  :", len(block2.hash))
+
+print("\n========== Block 3 ==========")
+print("Block Data   :", block3.data)
+print("Previous Hash:", block3.previousHash)
+print("Current Hash :", block3.hash)
+print("Hash Length  :", len(block3.hash))
